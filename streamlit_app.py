58922461093a8acf56703bb3cab7950b2cd892e3
husker9295 <75435539+husker9295@users.noreply.github.com>
2022-07-28 10:00:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,6 @@ streamlit.dataframe(fruits_to_show)
 
 #Function
 def get_fruityvice_data (this_fruit_chose):
- #streamlit.write('The user entered ', fruit_choice)
  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_chose)
  # normalize the json to be readable 
  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
