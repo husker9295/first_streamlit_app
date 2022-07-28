7071c8630b78071f3c6c124c7431a9b540ca743a
husker9295 <75435539+husker9295@users.noreply.github.com>
2022-07-27 23:46:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,18 +30,20 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json())
-
-
-# normalize the json to be readable 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Display the table on the page
-streamlit.dataframe(fruityvice_normalized)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+       streamlit.error("Please select a fruit to get information.")
+    else:
+    #streamlit.write('The user entered ', fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # normalize the json to be readable 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # Display the table on the page
+    streamlit.dataframe(fruityvice_normalized)
+ expect URLError as e:
+    streamlit.error()
+ 
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
